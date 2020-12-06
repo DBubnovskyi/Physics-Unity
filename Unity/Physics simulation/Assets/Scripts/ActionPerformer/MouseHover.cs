9a7d5696d7d6287932a5b6679b2f7a64265e351c
DESKTOP-QS0B0VT\ADSAD <dima.bubnovskyi@gmail.com>
2020-12-06 15:44:25 +0200

    VS project init Add new scripts

---
+++ Unity/Physics simulation/Assets/Scripts/ActionPerformer/MouseHover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.ActionPerformer
+{
+    public class MouseHover : MonoBehaviour
+    {
+        public List<GameObject> Hover = null;
+
+        [Tooltip("Represent game objects that should execute actions on mouse click")]
+        public List<GameObject> ActionPerformer = null;
+
+        void OnMouseEnter()
+        {
+            Hover.ForEach(x => x.SetActive(!x.activeSelf));
+        }
+
+        void OnMouseExit()
+        {
+            Hover.ForEach(x => x.SetActive(!x.activeSelf));
+        }
+
+        void OnMouseDown()
+        {
+            ActionPerformer.ForEach(x => x.gameObject.GetComponent<BasePerformer>()?.PerfomeAction());
+        }
+    }
+}
